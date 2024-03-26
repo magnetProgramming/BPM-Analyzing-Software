bf44a192f2d432efd62009adf7e186f1590c9036
magnetProgramming <161734476+magnetProgramming@users.noreply.github.com>
2024-03-26 03:54:03 -0400

    Update bpm-detector.py

---
+++ bpm-detector.py
@@ -0,0 +1,18 @@
+import customtkinter
+import librosa
+
+app = customtkinter.CTk()
+
+appVersion = "Version 1.0"
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
+
+app.geometry("400x500")
+
+app.title("BPM Detector " + appVersion)
+
+
+
+app.mainloop()
+
