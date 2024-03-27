010e1b33f5b2d3a9db43ad38fff483292b5d8a9a
magnetProgramming <161734476+magnetProgramming@users.noreply.github.com>
2024-03-26 23:28:51 -0400

    Update bpm-detector.py

--- bpm-detector.py
+++ bpm-detector.py
@@ -14,7 +14,14 @@ app.resizable(width=False, height=False)
 app.title("BPM Detector " + appVersion)
 
 
+def chooseSongFileDialog():
+    return
 
+def detectBPM():
+    try:
+
+    ecept:
+        return error
 
 def about():
     aboutWindow = customtkinter.CTkToplevel()
@@ -27,6 +34,9 @@ def about():
     appName_VersionNameLabel = customtkinter.CTkLabel(master=aboutWindow, text="BPM Detector " + appVersion)
     appName_VersionNameLabel.pack()
 
+
+chooseSongButton = customtkinter.CTkButton(master=app, text="Choose Song")
+chooseSongButton.pack()
 
 detectBPM_Button = customtkinter.CTkButton(master=app, text="Detect BPM")
 detectBPM_Button.pack()
